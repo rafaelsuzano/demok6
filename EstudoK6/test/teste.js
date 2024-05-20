fef23abb1700e37702c9dbf868ae0ba0cd33978b
Rafael Suzano <rafaelsuzano@hotmail.com>
2024-05-20 10:42:10 -0300

    Add files via upload

---
+++ EstudoK6/test/teste.js
@@ -0,0 +1,28 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  scenarios: {
+    shared_iter_scenario: {
+      executor: "shared-iterations",
+      vus: 10,
+      iterations: 10,
+      startTime: "0s",
+    },
+    per_vu_scenario: {
+      executor: "per-vu-iterations",
+      vus: 10,
+      iterations: 10,
+      startTime: "10s",
+    },
+  },
+};
+
+
+
+
+export default function () {
+  const res = http.get('https://httpbin.test.k6.io/');
+  check(res, { 'status was 200': (r) => r.status == 200 });
+  sleep(1);
+}
